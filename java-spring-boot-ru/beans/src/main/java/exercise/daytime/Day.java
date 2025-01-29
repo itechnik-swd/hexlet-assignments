3c6436cbeb0f02166bdcbb9a81a507457b9cea28
@hexlet/cli <support@hexlet.io>
2025-01-29 19:21:20 +0300

    submit java-spring-boot-ru/beans

---
+++ java-spring-boot-ru/beans/src/main/java/exercise/daytime/Day.java
@@ -0,0 +1,12 @@
+package exercise.daytime;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+
+@Getter
+public class Day implements Daytime {
+    private final String name = "day";
+
+    // BEGIN
+
+    // END
+}
---
+++ java-spring-boot-ru/beans/src/main/java/exercise/daytime/Night.java
@@ -0,0 +1,12 @@
+package exercise.daytime;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+
+@Getter
+public class Night implements Daytime {
+    private final String name = "night";
+
+    // BEGIN
+    
+    // END
+}
